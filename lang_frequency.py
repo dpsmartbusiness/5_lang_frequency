7c9498a8ae21c280a9f8b2e366655b0ba2d9a2a9
dpsmartbusiness <33832856+dpsmartbusiness@users.noreply.github.com>
2018-01-16 17:30:54 +0500

    add word search and output results

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
+import re
+import sys
+import collections
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r', encoding='ANSI') as text:
+        read_text = text.read()
+    return read_text
 
 
 def get_most_frequent_words(text):
-    pass
+    text = re.split(r'\W+', text)
+    duplicate_words_counter = collections.Counter(text)
+    return duplicate_words_counter.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        top_frequent_words = get_most_frequent_words(load_data(sys.argv[1]))
+        print ("Список из 10 слов наиболее часто встречающихся в тексте: \n")
+        for word,frequency in top_frequent_words:
+            print('Cлово: {} | Частота: {}'.format(word, frequency))
+    except FileNotFoundError:
+        print('Некорректный путь к файлу.')
+    except IndexError:
+        print('Укажите путь к файлу.')
+
+
+
+
